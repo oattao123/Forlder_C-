7b8e29690911ffc9cc9a6b1b7df549232042a0a6
oattao123 <72804848+oattao123@users.noreply.github.com>
2021-06-04 17:45:15 +0700

    Delete C directory

--- C/coding.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Enter char: %c\n",'c');
-    printf("Enter number: %d\n",10);
-    printf("Enter float number: %f\n",1.23);
-    printf("Enter string: %s\n","hello");
-}
