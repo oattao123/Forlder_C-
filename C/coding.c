2bef608cd73a7fb958cca481aeb24928ad0498b3
oattao123 <72804848+oattao123@users.noreply.github.com>
2021-06-04 17:43:25 +0700

    Add files via upload

---
+++ C/coding.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Enter char: %c\n",'c');
+    printf("Enter number: %d\n",10);
+    printf("Enter float number: %f\n",1.23);
+    printf("Enter string: %s\n","hello");
+}
