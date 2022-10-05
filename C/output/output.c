22cd20566599fe9e5ae0e12cc427e1eb0e91a399
oattao123 <dceriythrrmkic@gmail.com>
2022-10-05 23:15:47 +0700

    first

---
+++ C/output/output.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    //Output
+    printf("C Programming\n");
+
+    //Integer Output
+    int testInteger = 5;
+    printf("Number = %d\n", testInteger);
+
+    //float and double Output
+    float number1 = 13.5;
+    double number2 = 12.4;
+
+    printf("number1 = %f\n",number1);
+    printf("number2 = %f\n",number2);
+    
+    //Print Characters
+    char chr = 'a';
+    printf("characters = %c\n",chr);
+    
+    }
